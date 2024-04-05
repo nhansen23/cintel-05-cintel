b9b193d99dfa7c8138b56db86380f1fdb45a4b48
nhansen23 <156024214+nhansen23@users.noreply.github.com>
2024-04-05 18:56:20 -0500

    Updates to final formatting and additional data grid column

--- dashboard/app.py
+++ dashboard/app.py
@@ -31,9 +31,10 @@ def reactive_calc_combined():
     reactive.invalidate_later(UPDATE_INTERVAL_SECS)
 
     # Data generation code
-    temp = round(random.uniform(74, 99), 1)
+    temp = round(random.uniform(70, 99), 1)
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    new_dictionary_entry = {"temp": temp, "timestamp": timestamp}
+    temp_msg = "cooler than usual" if temp < 76.0 else "warmer than usual"
+    new_dictionary_entry = {"Temperature": temp, "Time Stamp": timestamp, "Temp Flag": temp_msg}
 
     # Get the deque and append the new entry
     reactive_value_wrapper.get().append(new_dictionary_entry)
@@ -46,9 +47,7 @@ def reactive_calc_combined():
 
     # For Display: get the latest dictionary entry
     latest_dictionary_entry = new_dictionary_entry
-
-    temp_msg = "cooler than usual" if temp < 76.0 else "warmer than usual"
-        
+    
     # Return a tuple with everything
     return deque_snapshot, df, latest_dictionary_entry
 
@@ -85,20 +84,18 @@ with ui.layout_columns():
         def display_temp():
             """Get the latest reading and return a temperature string"""
             deque_snapshot, df, latest_dictionary_entry = reactive_calc_combined()
-            return f"{latest_dictionary_entry['temp']} C"
-            
-            # print(temp_msg)
-        
+            return f"{latest_dictionary_entry['Temperature']} C"
+
     with ui.card(full_screen=True, min_height="40%"):
         ui.card_header("Current Date and Time")
         @render.text
         def display_time():
             """Get the latest reading and return a timestamp string"""
             deque_snapshot, df, latest_dictionary_entry = reactive_calc_combined()
-            return f"{latest_dictionary_entry['timestamp']}"
+            return f"{latest_dictionary_entry['Time Stamp']}"
     
 with ui.card(full_screen=True, min_height="30%"):
-        ui.card_header("Most Recent Readings")
+        ui.card_header("Most Recent Temperature Readings")
 
         @render.data_frame
         def display_df():
@@ -108,7 +105,7 @@ with ui.card(full_screen=True, min_height="30%"):
             return render.DataGrid( df,width="80%")
   
 with ui.card(full_screen=True):
-        ui.card_header("Chart with Current Trend")
+        ui.card_header("Chart with Current Temperature Trend")
 
         @render_plotly
         def display_plot():
@@ -118,14 +115,14 @@ with ui.card(full_screen=True):
             # Ensure the DataFrame is not empty before plotting
             if not df.empty:
                 # Convert the 'timestamp' column to datetime for better plotting
-                df["timestamp"] = pd.to_datetime(df["timestamp"])
+                df["Time Stamp"] = pd.to_datetime(df["Time Stamp"])
 
             # Create scatter plot for readings
             fig = px.scatter(df,
-                x="timestamp",
-                y="temp",
+                x="Time Stamp",
+                y="Temperature",
                 title="Temperature Readings with Regression Line",
-                labels={"temp": "Temperature (°C)", "timestamp": "Time"},
+                labels={"Temperature": "Temperature (°C)", "Time Stamp": "Time"},
                 color_discrete_sequence=["black"] )
             
                 # Linear regression - we need to get a list of the
@@ -136,13 +133,13 @@ with ui.card(full_screen=True):
                 # For x let's generate a sequence of integers from 0 to len(df)
             sequence = range(len(df))
             x_vals = list(sequence)
-            y_vals = df["temp"]
+            y_vals = df["Temperature"]
 
             slope, intercept, r_value, p_value, std_err = stats.linregress(x_vals, y_vals)
             df['best_fit_line'] = [slope * x + intercept for x in x_vals]
 
             # Add the regression line to the figure
-            fig.add_scatter(x=df["timestamp"], y=df['best_fit_line'], mode='lines', name='Regression Line')
+            fig.add_scatter(x=df["Time Stamp"], y=df['best_fit_line'], mode='lines', name='Regression Line')
 
             # Update layout as needed to customize further
             fig.update_layout(xaxis_title="Time",yaxis_title="Temperature (°C)")
